a2d9a87da0eaadd0cc65fc118e0373aec4daec28
Raj Prajapati <raazpz3479@gmail.com>
2026-06-15 00:03:44 +0545

    register section hasbeen created using reusable components

--- src/components/landingPage/rightsidecomponent/RegisterSection.tsx
+++ src/components/landingPage/rightsidecomponent/RegisterSection.tsx
@@ -1,4 +1,7 @@
 import { Link } from "react-router"
+import FormLabel from "../../ui/form/formlabel"
+import FormInputField from "../../ui/form/input"
+import FormButton from "../../ui/form/formbutton"
 export default function RegisterSection(){
     return(<>
     
@@ -6,22 +9,28 @@ export default function RegisterSection(){
         <section className="w-2/5 h-screen bg-mist-400">
           <section className=" w-full h-full flex justify-center items-center">
               <div className=" bg-gray-500/40  w-100 h-150 rounded-2xl shadow-lg p-10 text-2xl flex flex-col gap-20">
-                  <span className="">Register Account</span>
+
+                  <FormLabel className="text-2xl" >Sign up</FormLabel>
             
                     <div className="flex flex-col gap-7">
-                        <input type="text" className=" border-2 rounded-full  w-70 h-12 text-sm p-3" placeholder="Enter Name"></input>
-
-                       <input type="text" className=" border-2 rounded-full  w-70 h-12 text-sm p-3" placeholder="Enter Email"></input>
-                        <input type="text" className=" border-2 rounded-full  w-70 h-12 text-sm p-3" placeholder="Enter Password"></input>
+                        <FormInputField type="text" placeholder="enter your name"/>
+                        <FormInputField type="text" placeholder="enter email"/>
+                        <FormInputField type="password" placeholder="create a passsword"/>
                      </div>
+                     
                      <div className="flex justify-center items-center h-10 bg-blue-600 rounded-full text-xl hover:bg-sky-700 cursor-pointer">
-                         <button  className="cursor-pointer p-2" >Register</button>
+                         <FormButton navigateWhere="/home">Register</FormButton>
+                        
                      </div>
-                      <p className="text-lg text-center text-gray-800">
-                             Already have an account?{" "}
+                     <FormLabel className="text-lg text-center text-gray-800">
+                        Already have an account?{" "}
                         <Link to="/" className="text-blue-800 cursor-pointer">
-                        Login</Link>
-        </p>
+                        Login
+                        </Link>
+                        </FormLabel>
+                      
+                             
+        
                  </div>
              </section>
          </section>
